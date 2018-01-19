c926bd5fb842fccc43ac63344304de1b9da848c9
tiaotiao <tiaotiaoyly@gmail.com>
2018-01-18 23:41:55 -0600

    accepted 100-same-tree

---
+++ 100-same-tree.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        
+        if (p.val != q.val) {
+            return false;
+        }
+        
+        boolean left = isSameTree(p.left, q.left);
+        if (!left) {
+            return false;
+        }
+        boolean right = isSameTree(p.right, q.right);
+        if (!right) {
+            return false;
+        }
+        
+        return true;
+    }
+}
