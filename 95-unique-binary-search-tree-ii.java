9345e4d9ac038092bb2a4b924c6da62238aa38b4
tiaotiao <tiaotiaoyly@gmail.com>
2018-06-12 00:59:26 -0500

    accepted 95 unique binary search tree ii

---
+++ 95-unique-binary-search-tree-ii.java
@@ -0,0 +1,65 @@
+/*
+Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+
+Example:
+
+Input: 3
+Output:
+[
+  [1,null,3,2],
+  [3,2,null,1],
+  [3,1,null,null,2],
+  [2,1,3],
+  [1,null,2,null,3]
+]
+Explanation:
+The above output corresponds to the 5 unique BST's shown below:
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+*/
+
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return generate(1, n);
+    }
+
+    private List<TreeNode> generate(int from, int to) {
+        List<TreeNode> list = new ArrayList<>();
+        
+        if (from > to) {
+            list.add(null);
+            return list;
+        }
+
+        for (int i = from; i <= to; i++) {
+            List<TreeNode> lefts = generate(from, i-1);
+            List<TreeNode> rights = generate(i+1, to);
+
+            for (TreeNode left: lefts) {
+                for (TreeNode right: rights) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    list.add(root);
+                }
+            }
+        }
+
+        return list;
+    }
+}
