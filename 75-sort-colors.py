6429c4c49e8024cb45bd145ba13af04efab06cf6
tiaotiao <tiaotiaoyly@gmail.com>
2018-07-05 01:44:17 -0500

    accepted 75 sort color: counting sort, array, two pointers

--- 75-sort-colors.py
+++ 75-sort-colors.py
@@ -4,7 +4,24 @@ class Solution:
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        return self.countingSort(nums)
+        self.onePassSort(nums)
+
+    def onePassSort(self, nums):
+        left, right = 0, len(nums) - 1
+        i = 0
+        while i <= right:
+            num = nums[i]
+            if num == 0:
+                nums[left], nums[i] = nums[i], nums[left]
+                left += 1
+                i += 1
+            elif num == 1:
+                i += 1
+            else:   # num == 2
+                nums[right], nums[i] = nums[i], nums[right]
+                right -= 1
+
+        
         
     def countingSort(self, nums):
         count = [0, 0, 0]
@@ -15,3 +32,16 @@ class Solution:
             for _ in range(0, count[num]):
                 nums[i] = num
                 i += 1
+
+
+def main():
+    s = Solution()
+
+    nums = [2,0,1]
+
+    s.sortColors(nums)
+
+    print(nums)
+
+if __name__ == '__main__':
+    main()
