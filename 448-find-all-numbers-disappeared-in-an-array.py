55ced1bb430a223e46b3399f9d5e8354d6f1be95
tiaotiao <tiaotiaoyly@gmail.com>
2018-07-25 03:38:35 -0500

    448 find all numbers disappeared: array, tricky

---
+++ 448-find-all-numbers-disappeared-in-an-array.py
@@ -0,0 +1,32 @@
+# Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+# Find all the elements of [1, n] inclusive that do not appear in this array.
+
+# Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+# Example:
+
+# Input:
+# [4,3,2,7,8,2,3,1]
+
+# Output:
+# [5,6]
+
+class Solution:
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for v in nums:
+            idx = abs(v) - 1
+            nums[idx] = -abs(nums[idx])
+        return [i+1 for i in range(len(nums)) if nums[i] > 0]
+    
+def main():
+    s = Solution()
+    nums = [4,3,2,7,8,2,3,1]
+    print(s.findDisappearedNumbers(nums))
+
+if __name__ == '__main__':
+    main()
