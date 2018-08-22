ad54aa86397a49d8e38f2002177dfa3d1e0fc25f
tiaotiao <tiaotiaoyly@gmail.com>
2018-08-21 19:56:00 -0500

    209 minimum size subarray sum: two pointers, binary search.

--- 209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum.java
@@ -1,9 +1,80 @@
 
-// O(n)
+
 
 class Solution {
     
     public int minSubArrayLen(int s, int[] nums) {
+        return minSubArrayLen_besarch_len(s, nums);
+    }
+
+    // O(n) binary search
+    public int minSubArrayLen_besarch_len(int s, int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+
+        int sum = 0;
+        for (int num: nums) sum += num;
+        if (sum < s) return 0;
+
+        int min = 1, max = n;
+        
+        while (min < max) {
+            int len = (min + max) / 2;
+
+            // calc sums for window with size=len
+            boolean ok = false;
+            sum = 0;
+            for (int i = 0; i < n; i++) {
+                sum += nums[i];
+                if (i < len) {
+                    if (sum >= s) {
+                        ok = true;
+                        break;
+                    }
+                    continue;
+                }
+
+                sum -= nums[i - len];
+                if (sum >= s) {
+                    ok = true;
+                    break;
+                }
+            }
+
+            // bsearch
+            if (ok) {   // go smaller
+                max = len;
+            } else {
+                min = len + 1;
+            }
+        }
+
+        return min;
+    }
+
+    // O(n) two pointers
+    public int minSubArrayLen_two_pointers(int s, int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+
+        int sum = 0, minlen = 0;
+        int left=0, right=0;
+        while (right < n) {
+            sum += nums[right++];
+
+            while (sum >= s && left < right) {
+                if (minlen == 0 || minlen > right - left) {
+                    minlen = right - left;
+                }
+                sum -= nums[left++];
+            }
+        }
+
+        return minlen;
+    }
+
+    // O(n) prefix sum
+    public int minSubArrayLen_prefixsum(int s, int[] nums) {
         int n = nums.length;
         if (n <= 0) {
             return 0;
