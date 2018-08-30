f3439eff7c01d054556d67d7bdeebaf71f7a935f
tiaotiao <tiaotiaoyly@gmail.com>
2018-08-29 23:52:00 -0500

    97 interleaving string: DP, BFS

--- 97-interleaving-string.java
+++ 97-interleaving-string.java
@@ -3,6 +3,36 @@ import java.util.*;
 
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
+        return isInterleave_dp(s1, s2, s3);
+    }
+
+    public boolean isInterleave_dp(String s1, String s2, String s3) {
+        int n = s1.length();
+        int m = s2.length();
+        if (n + m != s3.length()) return false;
+        if (n == 0) return s2.equals(s3);
+        if (m == 0) return s1.equals(s3);
+        
+        // dp[i][j] = true if everything before s3[i+j] is matched at s1[i] and s2[j]
+        boolean[][] dp = new boolean[n+1][m+1];
+        dp[0][0] = true;
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0 && j == 0) continue;
+                int k = i + j - 1;
+                if (i > 0 && dp[i-1][j]) {
+                    if (s1.charAt(i-1) == s3.charAt(k)) dp[i][j] = true;
+                }
+                if (j > 0 && dp[i][j-1]) {
+                    if (s2.charAt(j-1) == s3.charAt(k)) dp[i][j] = true;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    public boolean isInterleave_bfs(String s1, String s2, String s3) {
         int n = s1.length();
         int m = s2.length();
         if (n + m != s3.length()) return false;
