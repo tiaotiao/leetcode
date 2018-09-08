46b2921bdc2682cfde7b18a3138e94f0bc13f391
tiaotiao <tiaotiaoyly@gmail.com>
2018-09-08 02:08:01 -0500

    4 median of two arrays: binary search

--- 4-median-of-two-arrays.java
+++ 4-median-of-two-arrays.java
@@ -1,22 +1,65 @@
 
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        return solution_binary_guess(nums1, nums2);
+        return solution_dropoff_one_fourth(nums1, nums2);
     }
 
-    private double solution_kth_smallest_divide_and_conquer(int[] nums1, int[] nums2) {
+    ///////////////////////////////////////////////////////////////////////////
 
+    private double solution_dropoff_one_fourth(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int n = len1 + len2;
+
+        if (n % 2 == 1) {
+            return search_kth(n/2, nums1, 0, len1-1, nums2, 0, len2-1);
+        } else {
+            int x = search_kth(n/2, nums1, 0, len1-1, nums2, 0, len2-1);
+            int y = search_kth(n/2-1, nums1, 0, len1-1, nums2, 0, len2-1);
+            // System.out.println("xy:" + x + " " + y);
+            return (x+y) / 2.0;
+        }
     }
 
-    private int kth_smallest(int k, int[] nums1, int idx1, int[] nums2, int idx2) {
-        int n = nums1.length;
-        int m = nums2.length;
-        if (n + m <= k) return -1;
+    private int search_kth(int k, int[] nums1, int l1, int r1, int[] nums2, int l2, int r2) {
         
-    }
+        int len1 = r1 - l1 + 1;
+        int len2 = r2 - l2 + 1;
 
-    // private double solution_dropoff_one_fourth(int[] nums1, int[] nums2) {
-    // }
+        // System.out.printf("search %d: [%d-%d](%d);  [%d-%d](%d)\n",k, l1, r1, len1, l2, r2, len2);
+
+        
+        if (len1 <= 0) {
+            return nums2[l2+k];
+        }
+        if (len2 <= 0) {
+            return nums1[l1+k];
+        }
+        
+        
+        int m1 = (l1+r1) / 2;
+        int m2 = (l2+r2) / 2;
+        int mid1 = nums1[m1];
+        int mid2 = nums2[m2];
+        
+        int leftSide = (m1-l1) + (m2-l2) + 1;
+
+        if (k >= leftSide) {
+            // dropoff one of the left side
+            if (mid1 < mid2) {
+                return search_kth(k - (m1-l1+1), nums1, m1+1, r1, nums2, l2, r2);
+            } else {
+                return search_kth(k - (m2-l2+1), nums1, l1, r1, nums2, m2+1, r2);
+            }
+        } else { // k < leftSide
+            // dropoff one of the right side
+            if (mid1 < mid2) {
+                return search_kth(k, nums1, l1, r1, nums2, l2, m2-1);
+            } else {
+                return search_kth(k, nums1, l1, m1-1, nums2, l2, r2);
+            }
+        }
+    }
 
     ///////////////////////////////////////////////////////////////
     // Guess a value, see if the value is the median. Use binary search to guess the answer.
